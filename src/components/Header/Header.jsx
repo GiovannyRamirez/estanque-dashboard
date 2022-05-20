fd2ef8356025b0cd4a9e4fc05f0556b482907dee
Giovanny <giovannyramirezs@hotmail.com>
2022-05-19 21:27:29 -0500

    Delete names in Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,10 +4,6 @@ import './style.css'
 const Header = () => (
   <div className="Header__container">
     <h1 className="Header__title">Laboratorio Estanque</h1>
-    <p className="Header__member">Lawrence Eliecer Montoya</p>
-    <p className="Header__member">Brayan Estiven Barinas</p>
-    <p className="Header__member">Daniel Ricardo Galván</p>
-    <p className="Header__member">Anjhelo Giovanny Ramírez</p>
   </div>
 )
 
